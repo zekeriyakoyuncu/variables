16f6cc6d568b98280caa99c2bb9d4a6715d42b2d
zeker <zeker@MSI>
2022-10-02 05:43:15 +0300

    String and integer variables added.

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,14 @@
+package variables;
+//reusbility(tekrar tekrar kullanılabilirlik)
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayisi: ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}
